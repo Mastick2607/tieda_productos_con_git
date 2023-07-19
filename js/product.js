e0302e228e97a8bb02d15f5215f951b377a72625
marlon <mstick2607@gmail.com>
2023-07-19 10:04:27 -0500

    subir tienda con git

---
+++ js/product.js
@@ -0,0 +1,27 @@
+const productos =[
+    {
+     id:1,
+    nombre:"harinas",
+    precio:50,
+    img:"https://s.cornershopapp.com/product-images/1059679.jpg?versionId=Qfcqih2VEtzeBDxOXhAaRMoHltcy2AyL",
+  
+    },
+    {
+      id:2, 
+     nombre:"Galletas",
+     precio:100,
+     img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXb-BXtlJq0KGvQgzIRlfQM5FXsSoz0Ju4mA&usqp=CAU"
+   
+     },
+  
+  
+     {
+      id:3,
+     nombre:"gaseosa",
+     precio:2.500,
+     img:"https://licoresmedellin.com/cdn/shop/products/gaseosa-manzana-postobon-grande-25lgaseosa-manzana-postobon-grande-25lpostobonlicores-medellin-362264.png?v=1681661908&width=1024",
+   
+     }
+  
+  
+  ];
